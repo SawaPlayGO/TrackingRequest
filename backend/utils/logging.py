ed78df0c8b1cd4ee847bee9f80006ef8294af07b
Alexander Glumov <100450913+SawaPlayGO@users.noreply.github.com>
2026-07-02 23:35:23 +0700

    FEAT: logging, lifespan (#2)
    * feat: logging, lifespan
    
    * fix: ruff linting

---
+++ backend/utils/logging.py
@@ -0,0 +1,9 @@
+import structlog
+from structlog.typing import FilteringBoundLogger
+
+structlog.configure(
+    processors=[structlog.processors.JSONRenderer()],
+    logger_factory=structlog.BytesLoggerFactory(),
+)
+
+logger: FilteringBoundLogger = structlog.get_logger()
