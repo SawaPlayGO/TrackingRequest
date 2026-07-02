9aafde9f917960e60f4e0081135bd74fb8485487
Alexander Glumov <100450913+SawaPlayGO@users.noreply.github.com>
2026-07-02 23:35:23 +0700

    FEAT: logging, lifespan (#2)
    * feat: logging, lifespan
    
    * fix: ruff linting

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from config import settings
+from utils.logging import logger
+from database import engine
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await logger.ainfo("Application is starting up...", version="1.0.0")
+    yield
+    await logger.ainfo("Application is shutting down...")
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/health_check")
